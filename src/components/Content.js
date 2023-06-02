940b3d04cc20d59982538de8fb747e23bea87f31
kd00009 <disojakaran@gmail.com>
2023-06-02 18:12:58 +0530

    Your commit message

--- src/components/Content.js
+++ src/components/Content.js
@@ -4,13 +4,35 @@ const Content = () => {
   return (
   <>
     <div><h3 className='text-gray-600'>Sip calculator - Systematic Invetmsnet plan calculator</h3>
-    <p className='text-gray-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit nam minima accusantium nihil non tenetur enim et, praesentium iste accusamus placeat suscipit ipsam doloribus recusandae reprehenderit. Delectus ducimus ratione saepe rem facilis porro vitae sint illum debitis, sequi laborum optio id! Rem voluptates optio, unde alias doloribus eveniet? Impedit libero at unde totam. Soluta, ipsam!</p>
+    <p className='text-gray-700'>Prospective investors can think that SIPs and mutual funds are the same. However, SIPs are merely a method of investing in mutual funds, the other method being a lump sum. A SIP calculator is a tool that helps you determine the returns you can avail when parking your funds in such investment tools. Systematic Investment Plan or SIP is a process of investing a fixed sum of money in mutual funds at regular intervals. SIPs usually allow you to invest weekly, quarterly, or monthly. </p>
     </div>
-    <div><h3 className='text-gray-600'>Sip calculator - Systematic Invetmsnet plan calculator</h3>
-    <p className='text-gray-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit nam minima accusantium nihil non tenetur enim et, praesentium iste accusamus placeat suscipit ipsam doloribus recusandae reprehenderit. Delectus ducimus ratione saepe rem facilis porro vitae sint illum debitis, sequi laborum optio id! Rem voluptates optio, unde alias doloribus eveniet? Impedit libero at unde totam. Soluta, ipsam!</p>
+    <div><h3 className='text-gray-600'>What is a SIP Calculator?</h3>
+    <p className='text-gray-700'>A SIP calculator is a simple tool that allows individuals to get an idea of the returns on their mutual fund investments made through SIP. SIP investments in mutual funds have become one of the most popular investment options for millennials lately.
+
+These mutual fund sip calculators are designed to give potential investors an estimate on their mutual fund investments. However, the actual returns offered by a mutual fund scheme varies depending on various factors. The SIP calculator does not provide clarification for the exit load and expense ratio (if any).
+
+This calculator will calculate the wealth gain and expected returns for your monthly SIP investment. Indeed, you get a rough estimate on the maturity amount for any of your monthly SIP, based on a projected annual return rate.</p>
     </div>
-    <div><h3 className='text-gray-600'>Sip calculator - Systematic Invetmsnet plan calculator</h3>
-    <p className='text-gray-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit nam minima accusantium nihil non tenetur enim et, praesentium iste accusamus placeat suscipit ipsam doloribus recusandae reprehenderit. Delectus ducimus ratione saepe rem facilis porro vitae sint illum debitis, sequi laborum optio id! Rem voluptates optio, unde alias doloribus eveniet? Impedit libero at unde totam. Soluta, ipsam!</p>
+    <div><h3 className='text-gray-600'>How can a SIP return calculator help you?</h3>
+    <p className='text-gray-700'>Ps are a more lucrative mode of investing funds compared to a lump sum amount according to several mutual fund experts. It helps you become financially disciplined and create a habit of savings that can benefit you in the future.
+
+A SIP calculator online is a beneficial tool, which shows the estimated returns you will earn after the investment tenure.
+
+Few of the benefits of SIP calculators include –
+
+Assists you to determine the amount you want to invest in.
+Tells you the total amount you have invested.
+Gives an estimated value of the returns.</p>
+    </div>
+    <div><h3 className='text-gray-600'>Advantages of using Groww systematic investment plan calculator</h3>
+    <p className='text-gray-700'>Groww offers the best SIP calculator, which provides the following advantages :
+<ul>
+<li>Plan your investment based on the amount and tenure.</li>
+<li>It helps you compute an estimation of the total value of investments at the end of your SIP tenure.</li>
+<li>Shows accurate results and helps you save time required during a manual calculation.</li>
+<li>A systematic investment plan calculator ensures that your savings portfolio is as per your requirements and financial needs.</li>
+</ul>
+</p>
     </div>
 
     </>
